cc73657dd296d1bf84697ce63f7df4658fe94b7c
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-06 14:30:58 +0400

    laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,73 @@
+<!-- resources/views/welcome.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Lovely Planet</title>
+    <!-- Include CSS Files -->
+    <link href="{{ asset('lugada/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lugada/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('lugada/css/magnific-popup.css') }}" rel="stylesheet">
+    <link href="{{ asset('lugada/css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lugada/css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Include the template's HTML content here -->
+    <!-- Example of including a logo and navigation -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="top-container">
+                <!-- Logo -->
+                <a href="#" class="logo" title="Home"><img src="{{ asset('lugada/img/logo.png') }}" alt=""></a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Navigation -->
+    <div class="navbar navbar-custom" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <i class="ion-navicon"></i>
+                </button>
+                <a class="navbar-brand logo" href="index.html">
+                    <i class="ion-social-buffer"></i>
+                    <span>Lugada</span>
+                </a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#features">Features</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#screenshots">Screenshots</a></li>
+                    <li><a href="#pricing">Pricing</a></li>
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown">Pages <span class="caret"></span></a>
+                        <ul class="dropdown-menu arrow">
+                            <li><a href="about.html">About Us</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="faq.html">Faq</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- Include JavaScript Files -->
+    <script src="{{ asset('lugada/js/jquery.js') }}"></script>
+    <script src="{{ asset('lugada/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('lugada/js/jquery.easing.1.3.min.js') }}"></script>
+    <script src="{{ asset('lugada/js/SmoothScroll.js') }}"></script>
+    <script src="{{ asset('lugada/js/wow.min.js') }}"></script>
+    <script src="{{ asset('lugada/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('lugada/js/jquery.countdown.js') }}"></script>
+    <script src="{{ asset('lugada/js/jquery.app.js') }}"></script>
+</body>
+
+</html>
