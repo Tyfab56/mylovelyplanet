b55025858a2665e277b5a95d6951c6ee72230fa3
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-06 17:33:48 +0400

    mlp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,30 +5,75 @@
         <div class="row justify-content-center">
             <div class="col-lg-9 text-center">
                 <div class="px-md-2">
-                    <img src="{{ asset('lugada/images/logosquare.png') }}">
+                    <img class="" src="{{ asset('lugada/images/logosquare.png') }}">
                     <h2 class="text-white fw-bold f-36 mt-3 pb-2 animated fadeInDown wow" data-wow-delay=".1s">Explorez le monde avec My Lovely Planet</h2>
-                    <p class="text-white f-16 mb-5 mt-4 animated fadeInDown wow" data-wow-delay=".2s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat arcu ut orci porta, eget porttitor felis suscipit. Sed a nisl ullamcorper, tempus augue at, rutrum lacus. Duis et turpis eros.</p>
-                    <a href="javascript:void(0);" class="btn btn-primary rounded-pill animated fadeInDown wow" data-wow-delay=".4s">Learn more</a>
+                    <p class="text-white f-16 mb-5 mt-4 animated fadeInDown wow" data-wow-delay=".2s">Nous concevons des outils d'exploration pour explolrer la planète : Cartes interactives, Audioguides, Exposition en Réalitée augmentée, Vues 360°, outils d'assistance aux photographes </p>
 
-                    <div class="watch-video">
-                        <a href="" class="btn video-btn text-white popup-video animated fadeInDown wow" data-wow-delay=".6s" data-bs-toggle="modal" data-bs-target="#watchvideomodal"><i class="ion-play me-2"></i>Watch Video</a>
 
-                        <div class="modal fade bd-example-modal-lg" id="watchvideomodal" data-keyboard="false" tabindex="-1" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered modal-dialog modal-lg">
-                                <div class="modal-content video-modal">
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    <video id="VisaChipCardVideo" class="w-100" controls>
-                                        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
-                                        <!--Browser does not support <video> tag -->
-                                    </video>
-                                </div>
+                </div>
+            </div>
+        </div>
+</section>
+<section class="py-28">
+    <div class="container">
+        <div class="max-w-md  mx-auto text-center">
+            <h2 class="text-3xl font-bold">Blog Section 6</h2>
+            <p class="text-base font-medium text-gray-500 mt-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div><!-- end max-w -->
+
+        <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-6 mt-14">
+            <div class="shadow bg-white rounded-md">
+                <div class="relative">
+                    <div class="absolute top-0 start-0 p-2">
+                        <span class="inline-flex items-center justify-center gap-2 bg-white/20 backdrop-blur-xl text-[13px] font-medium rounded-full text-white py-1 px-2"><span class="w-1.5 h-1.5 inline-block bg-white rounded-full"></span> 12 Aug 2022</span>
+                    </div>
+                    <img src="assets/images/blog-17.jpg" class="rounded-md" alt="">
+                    <div class="absolute inset-x-0 top-auto bottom-0 p-2">
+                        <div class="bg-white/50 backdrop-blur-xl p-4 rounded-md">
+                            <div class="text-white text-center">
+                                <h4 class="text-2xl font-semibold">Sales buildings</h4>
+                                <p class="text-base font-medium mt-3">Lorenm ipsum dolor sit amet consectetur. Vitae <a href="#" class="underline">Read more</a></p>
                             </div>
                         </div>
-                        <!-- Modal -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="shadow bg-white rounded-md">
+                <div class="relative">
+                    <div class="absolute top-0 start-0 p-2">
+                        <span class="inline-flex items-center justify-center gap-2 bg-white/20 backdrop-blur-xl text-[13px] font-medium rounded-full text-white py-1 px-2"><span class="w-1.5 h-1.5 inline-block bg-white rounded-full"></span> 15 Aug 2022</span>
+                    </div>
+                    <img src="assets/images/blog-18.jpg" class="rounded-md" alt="">
+                    <div class="absolute inset-x-0 top-auto bottom-0 p-2">
+                        <div class="bg-white/50 backdrop-blur-xl p-4 rounded-md">
+                            <div class="text-white text-center">
+                                <h4 class="text-2xl font-semibold">Read buildings</h4>
+                                <p class="text-base font-medium mt-3">Lorenm ipsum dolor sit amet consectetur. Vitae <a href="#" class="underline">Read more</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="shadow bg-white rounded-md">
+                <div class="relative">
+                    <div class="absolute top-0 start-0 p-2">
+                        <span class="inline-flex items-center justify-center gap-2 bg-white/20 backdrop-blur-xl text-[13px] font-medium rounded-full text-white py-1 px-2"><span class="w-1.5 h-1.5 inline-block bg-white rounded-full"></span> 22 Aug 2022</span>
+                    </div>
+                    <img src="assets/images/blog-19.jpg" class="rounded-md" alt="">
+                    <div class="absolute inset-x-0 top-auto bottom-0 p-2">
+                        <div class="bg-white/50 backdrop-blur-xl p-4 rounded-md">
+                            <div class="text-white text-center">
+                                <h4 class="text-2xl font-semibold">Buy buildings</h4>
+                                <p class="text-base font-medium mt-3">Lorenm ipsum dolor sit amet consectetur. Vitae <a href="#" class="underline">Read more</a></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </section>
 @endsection
