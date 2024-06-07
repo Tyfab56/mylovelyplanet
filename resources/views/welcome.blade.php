11c5ae725d549f5fd7d6cd148d39969f47fb9bda
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-07 09:51:18 +0400

    mlp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,8 +26,8 @@
         <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-14">
             <div class="p-6 rounded-lg bg-card">
 
-                <h2 class="text-xl font-bold mt-5">Site Internet : My Secret Map</h2>
-                <div class="flex items-center gap-3 mt-4">
+                <h2 class="text-xl font-bold">Site Internet : My Secret Map</h2>
+                <div class="flex items-center gap-3 mt-2 mb-2">
 
                     <div>
                         <h4 class="text-gray-800">Chaque année de nouvelles destinations</h4>
