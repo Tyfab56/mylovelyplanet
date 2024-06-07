8116b8852ff4928b1b29b90da403c690dafea97d
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-07 09:08:27 +0400

    mlp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,15 +19,14 @@
 <section class="relative py-28 bg-cover bg-no-repeat" style="background-image:url({{ asset('lugada/images/blog-bg-1.png') }})">
     <div class="container">
         <div class="max-w-md  mx-auto text-center">
-            <h2 class="text-3xl font-bold">Blog Section 2</h2>
-            <p class="text-base font-medium text-gray-500 mt-4">Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry.</p>
+            <h2 class="text-3xl font-bold">Découvrez nos sites</h2>
+            <p class="text-base font-medium text-gray-500 mt-4">Cliquez sur les liens pour ouvrir dans une nouvelle page</p>
         </div>
 
         <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-14">
             <div class="p-6 rounded-lg bg-white">
                 <img src="assets/images/blog-4.jpg" class="rounded-lg" alt="">
-                <h2 class="text-xl font-bold mt-5">A Leading Global Agency In 15 Years.</h2>
+                <h2 class="text-xl font-bold mt-5">My Secret Map</h2>
                 <div class="flex items-center gap-3 mt-4">
                     <div>
                         <img src="assets/images/user/img-1.jpg" class="h-10 w-10 rounded-full" alt="">
@@ -37,10 +36,10 @@
                         <h5 class="text-gray-500">15-12-2023</h5>
                     </div>
                 </div>
-                <p class="text-base font-semibold text-gray-500 mt-5">Lorem ipsum dolor sit amet consectetur Eius iusto debitis doloremque nulla reiciendis illum.</p>
+                <p class="text-base font-semibold text-gray-500 mt-5">Un outil pour découvrir les plus beaux sites et activités d'une selection de destinations</p>
                 <div class="inline-block mt-6">
-                    <a href="#" class="py-2 px-6 rounded-full flex items-center gap-2 font-medium text-white bg-blue-500/90 hover:bg-blue-500 transition-all duration-500">
-                        Read more
+                    <a href="https://www.mysecretmap.com" target="_blank" class="py-2 px-6 rounded-full flex items-center gap-2 font-medium text-white bg-blue-500/90 hover:bg-blue-500 transition-all duration-500">
+                        Voir le site
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-white">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
@@ -103,66 +102,5 @@
     </div><!-- end container -->
 </section>
 
-<section class="py-card">
-    <div class="container">
-        <div class="max-w-md  mx-auto text-center">
-            <h2 class="text-3xl font-bold">Blog Section 6</h2>
-            <p class="text-base font-medium text-gray-500 mt-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-        </div><!-- end max-w -->
 
-        <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-6 mt-14">
-            <div class="shadow bg-white rounded-md">
-                <div class="relative">
-                    <div class="absolute top-0 start-0 p-2">
-                        <span class="inline-flex items-center justify-center gap-2 bg-white/20 backdrop-blur-xl text-[13px] font-medium rounded-full text-white py-1 px-2"><span class="w-1.5 h-1.5 inline-block bg-white rounded-full"></span> 12 Aug 2022</span>
-                    </div>
-                    <img src="assets/images/blog-17.jpg" class="rounded-md" alt="">
-                    <div class="absolute inset-x-0 top-auto bottom-0 p-2">
-                        <div class="bg-white/50 backdrop-blur-xl p-4 rounded-md">
-                            <div class="text-white text-center">
-                                <h4 class="text-2xl font-semibold">Sales buildings</h4>
-                                <p class="text-base font-medium mt-3">Lorenm ipsum dolor sit amet consectetur. Vitae <a href="#" class="underline">Read more</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="shadow bg-white rounded-md">
-                <div class="relative">
-                    <div class="absolute top-0 start-0 p-2">
-                        <span class="inline-flex items-center justify-center gap-2 bg-white/20 backdrop-blur-xl text-[13px] font-medium rounded-full text-white py-1 px-2"><span class="w-1.5 h-1.5 inline-block bg-white rounded-full"></span> 15 Aug 2022</span>
-                    </div>
-                    <img src="assets/images/blog-18.jpg" class="rounded-md" alt="">
-                    <div class="absolute inset-x-0 top-auto bottom-0 p-2">
-                        <div class="bg-white/50 backdrop-blur-xl p-4 rounded-md">
-                            <div class="text-white text-center">
-                                <h4 class="text-2xl font-semibold">Read buildings</h4>
-                                <p class="text-base font-medium mt-3">Lorenm ipsum dolor sit amet consectetur. Vitae <a href="#" class="underline">Read more</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="shadow bg-white rounded-md">
-                <div class="relative">
-                    <div class="absolute top-0 start-0 p-2">
-                        <span class="inline-flex items-center justify-center gap-2 bg-white/20 backdrop-blur-xl text-[13px] font-medium rounded-full text-white py-1 px-2"><span class="w-1.5 h-1.5 inline-block bg-white rounded-full"></span> 22 Aug 2022</span>
-                    </div>
-                    <img src="assets/images/blog-19.jpg" class="rounded-md" alt="">
-                    <div class="absolute inset-x-0 top-auto bottom-0 p-2">
-                        <div class="bg-white/50 backdrop-blur-xl p-4 rounded-md">
-                            <div class="text-white text-center">
-                                <h4 class="text-2xl font-semibold">Buy buildings</h4>
-                                <p class="text-base font-medium mt-3">Lorenm ipsum dolor sit amet consectetur. Vitae <a href="#" class="underline">Read more</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</section>
 @endsection
