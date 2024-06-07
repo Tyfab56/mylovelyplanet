2cf65da17c3787dae1539b031f92c5303b3c19a4
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-06-07 08:51:01 +0400

    mlp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,93 @@
             </div>
         </div>
 </section>
+<section class="relative py-28 bg-cover bg-no-repeat" style="background-image: url(assets/images/blog-bg-1.png);">
+    <div class="container">
+        <div class="max-w-md  mx-auto text-center">
+            <h2 class="text-3xl font-bold">Blog Section 2</h2>
+            <p class="text-base font-medium text-gray-500 mt-4">Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry.</p>
+        </div>
+
+        <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 mt-14">
+            <div class="p-6 rounded-lg bg-white">
+                <img src="assets/images/blog-4.jpg" class="rounded-lg" alt="">
+                <h2 class="text-xl font-bold mt-5">A Leading Global Agency In 15 Years.</h2>
+                <div class="flex items-center gap-3 mt-4">
+                    <div>
+                        <img src="assets/images/user/img-1.jpg" class="h-10 w-10 rounded-full" alt="">
+                    </div>
+                    <div>
+                        <h4 class="text-gray-800">Jhon catla</h4>
+                        <h5 class="text-gray-500">15-12-2023</h5>
+                    </div>
+                </div>
+                <p class="text-base font-semibold text-gray-500 mt-5">Lorem ipsum dolor sit amet consectetur Eius iusto debitis doloremque nulla reiciendis illum.</p>
+                <div class="inline-block mt-6">
+                    <a href="#" class="py-2 px-6 rounded-full flex items-center gap-2 font-medium text-white bg-blue-500/90 hover:bg-blue-500 transition-all duration-500">
+                        Read more
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-white">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+
+            <div class="p-6 rounded-lg bg-white">
+                <img src="assets/images/blog-5.jpg" class="rounded-lg" alt="">
+                <h2 class="text-xl font-bold mt-5">A Leading Global Agency In 15 Years.</h2>
+                <div class="flex items-center gap-3 mt-4">
+                    <div>
+                        <img src="assets/images/user/img-2.jpg" class="h-10 w-10 rounded-full" alt="">
+                    </div>
+                    <div>
+                        <h4 class="text-gray-800">Credon catla</h4>
+                        <h5 class="text-gray-500">15-12-2023</h5>
+                    </div>
+                </div>
+                <p class="text-base font-semibold text-gray-500 mt-5">Lorem ipsum dolor sit amet consectetur Eius iusto debitis doloremque nulla reiciendis illum.</p>
+                <div class="inline-block mt-6">
+                    <a href="#" class="py-2 px-6 rounded-full flex items-center gap-2 font-medium text-white bg-blue-500/90 hover:bg-blue-500 transition-all duration-500">
+                        Read more
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-white">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+
+            <div class="p-6 rounded-lg bg-white">
+                <img src="assets/images/blog-6.jpg" class="rounded-lg" alt="">
+                <h2 class="text-xl font-bold mt-5">Startup Leading Global Agency In 5 Years.</h2>
+                <div class="flex items-center gap-3 mt-4">
+                    <div>
+                        <img src="assets/images/user/img-3.jpg" class="h-10 w-10 rounded-full" alt="">
+                    </div>
+                    <div>
+                        <h4 class="text-gray-800">Jhon catla</h4>
+                        <h5 class="text-gray-500">15-9-2023</h5>
+                    </div>
+                </div>
+                <p class="text-base font-semibold text-gray-500 mt-5">Lorem ipsum dolor sit amet consectetur Eius iusto debitis doloremque nulla reiciendis illum.</p>
+                <div class="inline-block mt-6">
+                    <a href="#" class="py-2 px-6 rounded-full flex items-center gap-2 font-medium text-white bg-blue-500/90 hover:bg-blue-500 transition-all duration-500">
+                        Read more
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-white">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div><!-- end container -->
+</section>
+
 <section class="py-card">
     <div class="container">
         <div class="max-w-md  mx-auto text-center">
