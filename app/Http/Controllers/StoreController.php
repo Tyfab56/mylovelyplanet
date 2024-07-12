e5d903f5aa6f76be5172bdc9a3fa2ca6507d7d04
Fabrice H <64085875+Tyfab56@users.noreply.github.com>
2024-07-12 14:07:10 +0200

    tostore

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\tostore;
+use Illuminate\Support\Carbon;
+
+class StoreController extends Controller
+{
+    public function tostore(Request $request)
+    {
+
+        // redirection vers le store
+        return redirect('https://mysecretmap.myshopify.com/');
+    }
+}
